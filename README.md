e0401faa50873cc77686e3ba6079a72df8261e34
infosyspranay <infosyspranay@ooyala.com>
2017-05-03 15:01:01 -0700

    Use async keyword before function declaration to make sure it returns a promise Use await keyword before the fetch calls to make the code seem synchronous

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Git hub repo to accompany the egghead tutorial for async await keywords in Javascript
+- Make sure node version is >= 7.6.0
+- run comamnd 'node index.js'
--- index.js
+++ index.js
@@ -4,16 +4,16 @@
 
   const fetch = require('node-fetch');
 
-  function showGitHubber(handle) {
+  async function showGitHubber(handle) {
     const url = `https://api.github.com/users/${handle}`;
     console.log(url);
 
-    fetch(url)
-      .then(response => response.json())
-      .then(user => {
-        console.log(user.name);
-        console.log(user.location);
-      });
+    const response = await fetch(url);
+    const user = await response.json();
+
+    console.log(user.name);
+    console.log(user.location);
+      
   }
 
   showGitHubber('pranaykumar');
