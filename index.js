0ac1a278e3e407273be57829efa8580cad346d2e
infosyspranay <infosyspranay@ooyala.com>
2017-05-03 15:24:14 -0700

    Refactor to use async method from inside a class

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 
   const fetch = require('node-fetch');
 
-  const showGitHubber = async(handle) => {
-    const url = `https://api.github.com/users/${handle}`;
-    console.log(url);
-
-    const response = await fetch(url);
-    const user = await response.json();
-
-    return user;
+  class GitHubAPIClient {
+    async showGitHubber(handle) {
+      const url = `https://api.github.com/users/${handle}`;
+      const response = await fetch(url);
+      return await response.json();
+    }
   }
 
-(async () => {
-  const user = await showGitHubber('pranaykumar');
-  console.log(user.name);
-  console.log(user.location);
-})();
+
+  (async() => {
+    const client = new GitHubAPIClient();
+    const user = await client.showGitHubber('pranaykumar');
+    console.log(user.name);
+    console.log(user.location);
+  })();
 
 }());
